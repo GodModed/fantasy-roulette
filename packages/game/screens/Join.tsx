b5b05c7b40ba98d2e969fc566286abb4ad98f30d
Dylan <djlevy26@gmail.com>
2026-02-11 08:14:10 -0500

    organize game code

---
+++ packages/game/screens/Join.tsx
@@ -0,0 +1,79 @@
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import { GENERAL_STATE, SCREEN } from "common/types";
+import { Text, TextInputProps } from "react-native";
+import { Box } from "../components/ui/box";
+import { Input, InputField } from "../components/ui/input";
+import { Button } from "../components/ui/button";
+import EventSource from "react-native-sse";
+
+const API_URL = "http://10.212.46.68:3000";
+
+export default function Join({
+	globalState,
+	setGlobalState
+}: {
+	globalState: GENERAL_STATE,
+	setGlobalState: Dispatch<SetStateAction<GENERAL_STATE>>
+}) {
+
+
+	const codeRef = useRef<TextInputProps>(null); // TODO: Make use state
+	const nameRef = useRef<TextInputProps>(null); // TODO: make use state
+
+	const [waiting, setWaiting] = useState<boolean>(false);
+	useEffect(() => {
+		if (!waiting) return;
+		const es = new EventSource<"start">(API_URL + "/hostStream/" + codeRef.current?.value, {
+			lineEndingCharacter: "\n"
+		});
+		es.addEventListener("start", (event) => {
+			setGlobalState(s => ({
+				...s,
+				screen: "SOLO"
+			}));
+		});
+
+		return (() => {
+			es.removeAllEventListeners();
+			es.close();
+		});
+
+	}, [waiting]);
+
+	function onClick() {
+		fetch(API_URL + "/join/" + codeRef.current?.value + "/" + nameRef.current?.value).then((res) => {
+			if (res.status == 200) setWaiting(true);
+			else {
+				setWaiting(false);
+			}
+			// setGlobalState(s => ({
+			// 	...s,
+			// 	screen: nextScreen
+			// }));
+		})
+	}
+
+	return (
+		<>
+			<Box className="flex-row p-4 items-center">
+				<Input className="mx-4">
+					<InputField ref={codeRef} placeholder="Code"></InputField>
+				</Input>
+
+				<Input className="mx-4">
+					<InputField ref={nameRef} placeholder="Name"></InputField>
+				</Input>
+
+				<Button className="mx-4" onPress={onClick} disabled={waiting}>
+					<Text>Join</Text>
+				</Button>
+
+			</Box>
+
+			{waiting && (
+				<Text className="text-base text-white text-xl">Waiting for host to start the game...</Text>
+			)}
+
+		</>
+	)
+}
